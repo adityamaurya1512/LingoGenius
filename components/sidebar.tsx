9cb55ccc3acdff3484f4c026a1f9c750cf6eedf9
adityamaurya1512 <adityamaurya.ece18@itbhu.ac.in>
2024-07-12 16:57:40 +0530

    first commit

---
+++ components/sidebar.tsx
@@ -0,0 +1,14 @@
+import {cn} from "@/lib/utils"
+
+
+type Props={
+    className?:string;
+}
+
+export const Sidebar = ({className}:Props) =>{
+    return (
+        <div className="bg-blue-500 h-full lg:w-[256px] lg:fixed flex  left-0 top-0 px-4 border-r-2 flex-col">
+            Sidebar
+        </div>
+    )
+}
