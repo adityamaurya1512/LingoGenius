acf89dbfb84dcec5953ac0fd4f7e3282419e49d0
adityamaurya1512 <adityamaurya.ece18@itbhu.ac.in>
2024-07-14 20:46:17 +0530

    courses page and learn

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,11 +1,21 @@
  import { StickyWrapper } from "@/components/sticky-wrapper"
  import {FeedWrapper} from "@/components/feed-wrapper"
  import {Header} from "./header"
- const LearnPage=()=>{
+import { Userprogress } from "@/components/user-progress"
+import { getUserProgress } from "@/db/queries"
+ const LearnPage=async ()=>{
+  const userProgressData=getUserProgress()
+  const [userProgress] = await Promise.all([
+    userProgressData]
+  )
+   if(!userProgress || !userProgress.activeCourse)
+   {
+    redirect("/courses")
+   }
     return (
         <div className="flex flex-row-reverse gap-[48px] px-6">
           <StickyWrapper>
-            My sticky sidebar
+           <Userprogress activeCourse={{title:"Spanish",imageSrc:"/es.svg"}} hearts={5} points={100} hasActiveSubscription={false}/>
           </StickyWrapper>
           <FeedWrapper>
            <Header title="Spanish"/>
--- components/sideBarItem.tsx
+++ components/sideBarItem.tsx
@@ -16,7 +16,7 @@ export const SideBarItem=({label,iconSrc,href}:Props)=>{
     const pathname=usePathname();
     const active = pathname===href
     return (
-    <Button variant={active ? "sideBar" : "sideBarOutline"} className="jsutify-start h-[52px]">
+    <Button variant={active ? "sideBarOutline" : "sideBar"} className="justify-start h-[52px]">
         <Link href={href}>
         <Image src={iconSrc} alt={label} className="mr-5" height={32} width={32}/>
         </Link>
